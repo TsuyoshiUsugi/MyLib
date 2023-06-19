382680124de43de8690caddd0aa82e39e554a142
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-20 02:27:44 +0900

    [add] カスタムタグ用の拡張を追加

---
+++ TsuyoshiLibray/Packages/TsuyoshiLibrary/Editor/CustomTag/CustomTagCandiudatePopup.cs
@@ -0,0 +1,65 @@
+using System;
+using UnityEngine;
+using UnityEditor;
+using System.Collections.Generic;
+using System.Reflection;
+
+public class CustomTagCandiudatePopup : PopupWindowContent
+{
+    static private GUIStyle labelStyle;
+    static CustomTagCandiudatePopup()
+    {
+        labelStyle = new GUIStyle(EditorStyles.label);
+        labelStyle.richText = true;
+    }
+    private string currentText;
+    private List<string> candidates;
+
+    public CustomTagCandiudatePopup(string currentText)
+    {
+        if (currentText == null || currentText.Length == 0)
+        {
+            this.currentText = "";
+            candidates = new List<string>();
+        }
+        else
+        {
+            this.currentText = currentText;
+            candidates = CustomTagRepository.GetCandidates(currentText);
+        }
+    }
+
+    public override Vector2 GetWindowSize()
+    {
+        return new Vector2(300, EditorGUIUtility.singleLineHeight * candidates.Count);
+    }
+
+    /// <summary>
+    /// GUI•`‰æ
+    /// </summary>
+    public override void OnGUI(Rect rect)
+    {
+        Rect line = new Rect(rect);
+        line.height = EditorGUIUtility.singleLineHeight;
+        foreach (var t in candidates)
+        {
+            var txt = currentText.Length == 0 ? t : t.Replace(currentText, $"<color=cyan![register.gif](https://qiita-image-store.s3.ap-northeast-1.amazonaws.com/0/75035/32561793-39f0-8408-207e-148d61fe1589.gif)>{ currentText}</ color > ");
+            EditorGUI.LabelField(line, new GUIContent(txt), labelStyle);
+            line.y += EditorGUIUtility.singleLineHeight;
+        }
+    }
+
+    public void OpenModeless(Rect position)
+    {
+        // PopupWindow.Init(activatorRect, windowContent, locationPriorityOrder, showMode, giveFocus: true);
+        var method = typeof(PopupWindow).GetMethod("Init", BindingFlags.Instance | BindingFlags.NonPublic);
+
+        PopupWindow popWin = ScriptableObject.CreateInstance<PopupWindow>();
+        if (popWin != null)
+        {
+            var showMode = Enum.Parse(method.GetParameters()[3].ParameterType, "PopupMenu");
+            method.Invoke(popWin, new object[] { position, this, null, showMode, false });
+        }
+
+    }
+}
---
+++ TsuyoshiLibray/Packages/TsuyoshiLibrary/Editor/CustomTag/CustomTagDrawer.cs
@@ -0,0 +1,95 @@
+using UnityEngine;
+using UnityEditor;
+using TsuyoshiLibrary;
+
+/// <summary>
+/// Control Editor of SkillCategoryTagCollection
+/// </summary>
+[CustomPropertyDrawer(typeof(CustomTag), true)]
+public class CustomTagDrawer : PropertyDrawer
+{
+    private CustomTagCandiudatePopup toolTip;
+    static private GUIContent registButton = new GUIContent("Regist", "このタグをリポジトリに登録");
+    static private GUIStyle attentionStyle;
+    private int index = -1;
+    static CustomTagDrawer()
+    {
+        attentionStyle = new GUIStyle(EditorStyles.textField);
+        var orange = new Color(0.9f, 0.4f, 0);
+        attentionStyle.normal.textColor = orange;
+        attentionStyle.focused.textColor = orange;
+        attentionStyle.hover.textColor = orange;
+    }
+
+    private string lastControlForPopup;
+    public override void OnGUI(Rect position, SerializedProperty property, GUIContent label)
+    {
+        if (property == null || property.serializedObject == null) return;
+
+        // フォーカス制御のため名前をつける
+        string control_name = "Tag" + GUIUtility.GetControlID(FocusType.Keyboard).ToString();
+        GUI.SetNextControlName(control_name);
+
+        var tag = GetChildProperty(property, "Tag");
+        using (var check = new EditorGUI.ChangeCheckScope())
+        {
+            //　左側に表示するボタンの領域
+            var buttonArea = new Rect(position);
+            buttonArea.width = EditorStyles.toolbarButton.CalcSize(registButton).x;
+
+            // メインのTextFieldの領域
+            var mainArea = new Rect(position);
+            var shift = buttonArea.width + EditorGUIUtility.standardVerticalSpacing;
+            mainArea.x += shift;
+            mainArea.width -= shift;
+
+            // 既存のタグに一致するかどうか
+            var contains = tag.stringValue.Length == 0 || CustomTagRepository.Contains(tag.stringValue);
+            // テキストフィールドの描画、既存のタグかどうかでスタイル切り替え
+            tag.stringValue = EditorGUI.TextField(mainArea, tag.stringValue, contains ? EditorStyles.textField : attentionStyle);
+
+            string focused_name = GUI.GetNameOfFocusedControl();
+            if (check.changed)
+            {
+                if (tag.stringValue.Length != 0)
+                {
+                    // 候補のツールチップ表示
+                    var popRect = new Rect(position);
+                    toolTip = new CustomTagCandiudatePopup(tag.stringValue);
+                    toolTip.OpenModeless(popRect);
+                    // フォーカスを再設定する
+                    GUI.FocusControl(focused_name);
+                    lastControlForPopup = focused_name;
+                }
+            }
+            else if (toolTip != null && focused_name != lastControlForPopup)
+            {
+                // 不要なツールチップを消す
+                toolTip.editorWindow.Close();
+            }
+
+            // 未登録のタグなら、登録ボタンを出し、押したらリポジトリに登録できるようにする。
+            if (!contains && GUI.Button(buttonArea, registButton))
+            {
+                CustomTagRepository.Instance.AddNewTag(tag.stringValue);
+            }
+        }
+    }
+
+    public override float GetPropertyHeight(SerializedProperty property, GUIContent label)
+    {
+        return EditorGUIUtility.singleLineHeight;
+    }
+
+    private static SerializedProperty GetChildProperty(SerializedProperty parent, string name)
+    {
+        SerializedProperty child = parent.Copy();
+        child.Next(true);
+        do
+        {
+            if (child.name == name) return child;
+        }
+        while (child.Next(false));
+        return null;
+    }
+}
