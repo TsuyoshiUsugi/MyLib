1af3e65dbf26a817cbd0d93b47eefe68638d30d0
TsuyoshiUsugi <qesulive@gmail.com>
2023-06-20 00:59:53 +0900

    [add] 既存のクラスを一つのnamespaceにまとめた

--- TsuyoshiLibray/Packages/TsuyoshiLibrary/Runtime/SingletonMonobehavior.cs
+++ TsuyoshiLibray/Packages/TsuyoshiLibrary/Runtime/SingletonMonobehavior.cs
@@ -1,59 +1,62 @@
 using UnityEngine;
 
-/// <summary>
-/// MonoBehaviorを継承したシングルトンの基底クラス
-/// 
-/// 機能
-/// 自身のインスタンスが存在する場合はそれを返す
-/// 存在しない場合新たにオブジェクトを作成し、このクラスをつける
-/// 変数からシーン移行時にも引き継がれるようにする
-/// その場合Awakeをオーバーライドする
-/// 
-/// </summary>
-/// <typeparam name="T"></typeparam>
-public class SingletonMonobehavior<T> : MonoBehaviour where T : MonoBehaviour
+namespace TsuyoshiLibrary
 {
-    static T _instance;
-
-    [SerializeField] bool _dontDestroy = false;
-
-    public static T Instance
+    /// <summary>
+    /// MonoBehaviorを継承したシングルトンの基底クラス
+    /// 
+    /// 機能
+    /// 自身のインスタンスが存在する場合はそれを返す
+    /// 存在しない場合新たにオブジェクトを作成し、このクラスをつける
+    /// 変数からシーン移行時にも引き継がれるようにする
+    /// その場合Awakeをオーバーライドする
+    /// 
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public class SingletonMonobehavior<T> : MonoBehaviour where T : MonoBehaviour
     {
-        get
+        static T _instance;
+
+        [SerializeField] bool _dontDestroy = false;
+
+        public static T Instance
         {
-            if (_instance == null)
+            get
             {
-                var previous = FindObjectOfType(typeof(T));
-                if (previous != null)
+                if (_instance == null)
                 {
-                    _instance = (T)previous;
+                    var previous = FindObjectOfType(typeof(T));
+                    if (previous != null)
+                    {
+                        _instance = (T)previous;
+                    }
+                    else
+                    {
+                        var go = new GameObject(typeof(T).Name);
+                        _instance = go.AddComponent<T>();
+                    }
                 }
-                else
-                {
-                    var go = new GameObject(typeof(T).Name);
-                    _instance = go.AddComponent<T>();
-                }
+                return _instance;
             }
-            return _instance;
         }
-    }
 
-    protected virtual void Awake()
-    {
-        if (_dontDestroy) DontDestroyOnLoad(gameObject);
-
-        if (!_instance)
+        protected virtual void Awake()
         {
-            _instance = this as T;
-            return;
+            if (_dontDestroy) DontDestroyOnLoad(gameObject);
+
+            if (!_instance)
+            {
+                _instance = this as T;
+                return;
+            }
+            else if (Instance == this)
+            {
+                return;
+            }
+
+            Destroy(gameObject);
+
+
         }
-        else if (Instance == this)
-        {
-            return;
-        }
-
-        Destroy(gameObject);
-
-
     }
 }
